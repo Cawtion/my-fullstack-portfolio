e6a4bc0df886b693fc3f695d19875956f8d0fa88
cawtion <cakewalkuser1@gmail.com>
2025-12-02 14:28:47 -0500

    my-fullstack-portfolio

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let y;
+
+  let tabs = [
+    { name: "Projects", link: "#projects" },
+    { name: "About me", link: "#about" },
+  ];
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 flex items-center justify-between border border-solid ' + ( 
+  y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent'
+)}>
+  <h1 class="font-medium">
+     <b class="font-bold-poppins">Cory</b> Williams
+  </h1>
+  <div class="sm:flex items-center gap-4 hidden">
+    {#each tabs as tab, idex}
+      <a>
+        <p>{tab.name}</p>
+      </a>
+    {/each}
+  </div>
+</header>
